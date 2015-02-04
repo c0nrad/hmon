13b1fc3c076e70fb0d7632c21eb56cad97d0d6ef
stuartlarsen <stuartlarsen@yahoo-inc.com>
2015-02-04 16:19:46 -0500

    Added env port

--- hmon.go
+++ hmon.go
@@ -13,16 +13,20 @@ const (
 	DefaultMongoURI = "localhost:27017"
 	Database        = "hmon"
 	Collection      = "scans"
-
-	Port = ":8080"
 )
 
 var Session *mgo.Session
+var Port = ":8080"
 
 func main() {
 	mongoURI := os.Getenv("MONGOLAB_URI")
 	if mongoURI == "" {
 		mongoURI = DefaultMongoURI
+	}
+
+	port := os.Getenv("PORT")
+	if port != "" {
+		Port = port
 	}
 
 	session, err := mgo.Dial(mongoURI)
